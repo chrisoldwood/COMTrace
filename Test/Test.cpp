de1a12ceca61d3a6758beccf25330c765db472c5
Admin <admin@chrisoldwood.com>
2009-08-01 23:56:28 +0000

    Updated as the tests are now self-registering.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -9,31 +9,16 @@
 #include <WCL/AutoCom.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
-// The test group functions.
-
-extern void TestComPtr();
-extern void TestCustom();
-extern void TestDual();
-extern void TestDispatch();
-extern void TestErrorInfo();
-
-////////////////////////////////////////////////////////////////////////////////
 //! The entry point for the test harness.
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TEST_SUITE_BEGIN(argc, argv)
+	TEST_SUITE(argc, argv)
 	{
 		WCL::AutoCom oCom(COINIT_APARTMENTTHREADED);
 //		WCL::AutoCom oCom(COINIT_MULTITHREADED);
 
-		TEST_CASE(TestComPtr);
-		TEST_CASE(TestCustom);
-		TEST_CASE(TestDual);
-		TEST_CASE(TestDispatch);
-		TEST_CASE(TestErrorInfo);
-
-		Core::setTestRunFinalStatus(true);
+		TEST_SUITE_RUN();
 	}
 	TEST_SUITE_END
 }
