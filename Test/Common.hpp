6cda1f6b07e544c739b2f8a84fb53d4dad59c5c2
Chris Oldwood <gort@cix.co.uk>
2016-02-02 21:15:47 +0000

    Renamed stdafx.h to Common.hpp to bring consistency across components.

---
+++ Test/Common.hpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   Common.hpp
+//! \brief  File to include the most commonly used headers.
+//! \author Chris Oldwood
+
+// Check for previous inclusion
+#ifndef APP_COMMON_HPP
+#define APP_COMMON_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+#include <Core/Common.hpp>
+#include <WCL/Common.hpp>
+#include <iostream>
+#import "../COMTrace.tlb" raw_interfaces_only no_smart_pointers
+
+#endif // APP_COMMON_HPP
--- Test/TestDispatch.cpp
+++ Test/TestDispatch.cpp
@@ -3,7 +3,7 @@
 //! \brief  The unit tests for the automation interface.
 //! \author Chris Oldwood
 
-#include "stdafx.h"
+#include "Common.hpp"
 #include <Core/UnitTest.hpp>
 #include <WCL/Variant.hpp>
 #include <WCL/ComPtr.hpp>
